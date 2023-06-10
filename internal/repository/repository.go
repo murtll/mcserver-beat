edd2118fb65062836ddec722e2404feed14fd95b
murtll <jkoz@inbox.ru>
2023-06-10 16:47:12 +0300

    change loading info strategy

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,8 +3,6 @@ package repository
 import (
 	"context"
 	"log"
-	"sort"
-	"strconv"
 	"time"
 
 	"github.com/murtll/mcserver-beat/config"
@@ -18,11 +16,18 @@ var ctx = context.Background()
 func Store(names []string, ttl time.Duration) error {
 	key := utils.JSONTime(time.Now()).RoundHour().String()
 
+	names = []string{}
+
 	tmp := make([]interface{}, len(names))
 	for i, v := range names {
 		tmp[i] = v
 	}
-	err := client.SAdd(ctx, key, tmp...).Err()
+	var err error = nil
+	if (len(names) == 0) {
+		err = client.SAdd(ctx, key, []interface{}{0}...).Err()
+	} else {
+		err = client.SAdd(ctx, key, tmp...).Err()
+	}
 	if err != nil {
 		return err
 	}
@@ -35,32 +40,20 @@ func Store(names []string, ttl time.Duration) error {
 }
 
 func Load(count int) (map[int][]string, error) {
-	stringKeys, err := client.Keys(ctx, "*").Result()
-	if err != nil {
-		return nil, err
-	}
-
-	keys := make([]int, 0)
-	for _, stringK := range stringKeys {
-		k, err := strconv.Atoi(stringK)
-		if err != nil {
-			log.Default().Printf("Was not able to parse Redis key '%s': %s. Skipping.", stringK, err)
-			continue
-		}
-		keys = append(keys, k)
-	}
-
-	if count > len(keys) {
-		count = len(keys)
-	}
-
-	sort.Ints(keys)
-	keys = keys[len(keys) - count:]
-
 	result := make(map[int][]string)
-	for _, k := range keys {
-		result[k], err = client.SInter(ctx, strconv.Itoa(k)).Result()
+	for startTime := time.Now().Add(time.Duration(-count + 1) * time.Hour); 
+		startTime.Compare(time.Now()) <= 0; 
+		startTime = startTime.Add(time.Hour) {
+
+		key := utils.JSONTime(startTime).RoundHour().String()
+		k := int(time.Time(utils.JSONTime(startTime).RoundHour()).UnixMilli())
+		var err error
+		result[k], err = client.SInter(ctx, key).Result()
 		if err != nil {
+			if err == redis.Nil {
+				result[k] = []string{}
+				continue
+			}
 			log.Default().Printf("Was not able to get Redis key '%d': %s. Skipping.", k, err)
 			delete(result, k)
 			continue
