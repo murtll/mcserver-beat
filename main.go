23795a54f13091e7121886a4e66d93449dc7dd92
murtll <jkoz@inbox.ru>
2023-06-19 23:10:40 +0300

    feature: ability to split worker and rest api

--- main.go
+++ main.go
@@ -12,7 +12,11 @@ import (
 
 func main() {
 	var health bool
+	var beatOnly bool
+	var apiOnly bool
 	flag.BoolVar(&health, "health", false, "make healthcheck and exit.")
+	flag.BoolVar(&beatOnly, "beat-only", false, "start only beat worker.")
+	flag.BoolVar(&apiOnly, "api-only", false, "start only REST API.")
 	flag.Parse()
 
 	if health {
@@ -23,6 +27,15 @@ func main() {
 		}
 		fmt.Println(status)
 		os.Exit(0)
+	}
+
+	if beatOnly {
+		worker.Start()
+		os.Exit(0)
+	}
+
+	if apiOnly {
+		log.Fatal(rest.Start())
 	}
 
 	go worker.Start()
